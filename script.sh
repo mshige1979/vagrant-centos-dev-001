9f2149dc4777f1a3fba73b1a999e7bc576ed87b7
shigeharu matsumoto <m_shige1979@r2.dion.ne.jp>
2014-05-07 21:24:03 +0900

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# iptables
+sudo service iptables stop
+
+
 # epel,remi
 sudo rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm
 sudo rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
